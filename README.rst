3a40c1d5ded58b349624268b9ed79855ed5b1940
David Szotten <davidszotten@gmail.com>
2015-07-23 17:36:48 +0200

    initial add

---
+++ README.rst
@@ -0,0 +1,6 @@
+rdb
+---
+
+Copied from celery.contrib.rdb @514f23
+https://github.com/celery/celery/blob/514f23e03d103b093d40f9e73a687a001aec781f/celery/contrib/rdb.py
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rdb',
+
+    version='0.1.0',
+    description='Standalone copy of celery.contrib.rdb',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/davidszotten/rdb',
+
+    # Choose your license
+    license='BSD',
+
+    py_modules=['rdb'],
+    install_requires=['billiard'],
+
+)
